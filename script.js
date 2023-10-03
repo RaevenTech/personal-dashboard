be7c48a9f6a831813d9e76cb28bf2cdd3be11a52
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-10-03 17:47:50 +0100

    initial setup complete

---
+++ script.js
@@ -0,0 +1,10 @@
+const unsplashBaseURL = "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+
+const getUnsplashImage = async () => {
+  const resp = await fetch(unsplashBaseURL, {method:"GET"})
+  const respData = await resp.json()
+  document.body.style.backgroundImage = `url(${respData.urls.regular})`
+  document.getElementById("photo-author").innerText = `${respData.user.name}`
+  console.log(respData)
+}
+getUnsplashImage()
